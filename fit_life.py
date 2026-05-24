7940279c067d44fc75a5b670241a9f9841105e05
Kirill Korovin <kirill@korovin.ru>
2026-05-24 22:21:21 +0300

    FitLife - MVP версия 1.1

--- fit_life.py
+++ fit_life.py
@@ -1,31 +1,54 @@
-# Проект FitLife - MVP версия 1.0
+# Проект FitLife - MVP версия 1.1
+
 WATER_PER_KG = 30
 ML_IN_LITER = 1000
 
-# Ввод имени пользователя
-user_name = input("Здравствуйте, давайте познакомимся, как вас зовут?  ")
-# Ввод возраста
-user_age = int(input("Сообщите ваш возраст?  "))
-# Ввод веса
-user_wegtht = float(input("Ваш вес в кг?  "))
-# Ввод роста
+user_name = input("Здравствуйте, давайте познакомимся,"
+                  "как вас зовут?  ").title()
 try:
-    user_height = float(input("Ваш рост в метрах?  "))
+    user_age = int(input("Сообщите ваш возраст?  "))
 except ValueError:
-    user_height = float(input("Вводить данные нужно через точку,\
-например 1.75. Ваш рост в метрах?  "))
+    user_age = int(input("Введите количество полных лет "))
 
-# Расчет ИМТ: вес разделить на (рост в квадрате)
-bmi = user_wegtht / user_height**2
+user_weight = float(input("Ваш вес в кг?  ").replace(',', '.', 1))
 
-# Подсчет воды: вес * 30 мл
-water_needed = user_wegtht * WATER_PER_KG
+user_height = float(input("Ваш рост в метрах?  ").replace(',', '.', 1))
 
-# Вывод результатов расчета
+
+def calc_bmi(wegtht, height):
+    """
+        Возвращает индекс массы тела
+
+    Параметры:
+            weight (float): вес - число с плавающей точкой
+            height (float): рост - число с плавающей точкойbdfy
+
+    Возвращаемое значение:
+            (float): имт - число с плавающей точкой
+    """
+    return wegtht / height**2
+
+
+def calc_water_needed(weight):
+    """
+        Возвращает норму потребления воды
+
+    Параметры:
+            weight (float): вес - число с плавающей точкой
+
+    Возвращаемое значение:
+            (float): норма воды - число с плавающей точкой
+    """
+    return (weight * WATER_PER_KG) / ML_IN_LITER
+
+
+bmi = round(calc_bmi(user_weight, user_height), 1)
+water_needed = calc_water_needed(user_weight)
+
 print()
 print(f'Вот ваши показатели и рекомендации {user_name}:')
 print()
-print(f'Ваш возраст {user_age}, индекс массы тела {round(bmi, 1)}')
-print(f'Норма потребления воды {water_needed / ML_IN_LITER:.1f} л. в день')
+print(f'Ваш возраст {user_age}, индекс массы тела {bmi}')
+print(f'Норма потребления воды {water_needed:.1f} л. в день')
 print()
 print("Расчет окончен. Будьте здоровы!")
